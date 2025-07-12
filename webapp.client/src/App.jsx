d679f98da571ba2106a69b2f9b718ab9cd133e30
Leonidas810 <leonardo20010912@gmail.com>
2025-07-11 20:47:23 -0600

    Agregar archivos de proyecto.

---
+++ webapp.client/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+    return (
+        <div className="h-screen w-screen bg-red-900">
+            <div className="flex justify-center items-center">
+                Hola esta es mi app saludos wuu
+            </div>
+        </div>
+   )
+}
+
+export default App;
